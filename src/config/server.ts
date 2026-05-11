e15aeaf1cd0dcb866b58e1a9d49fa08c13493aea
Sergio <201801656@est.umss.edu>
2026-05-11 15:44:22 -0400

    Fix

--- src/config/server.ts
+++ src/config/server.ts
@@ -3,15 +3,8 @@ import cors from 'cors'
 import morgan from 'morgan'
 import compression from 'compression'
 import authenticationRoute from '../routes/authentication.routes'
-import productRoute from '../routes/product.routes'
-import saleRoute from '../routes/sale.routes'
-import locationRoute from '../routes/location.routes'
-import employeeRoute from '../routes/employee.routes'
-import roleRoute from '../routes/role.routes'
-import transferRoute from '../routes/transferencias.routes'
-import lineRoute from '../routes/line.routes'
 
-import { verifyToken } from '../middleware/auth.middleware'
+//import { verifyToken } from '../middleware/auth.middleware'
 
 const app = express()
 
@@ -22,12 +15,5 @@ app.use(express.json())
 app.use(urlencoded({ extended: true }))
 
 app.use('/api/authentication', authenticationRoute)
-app.use('/api/product',verifyToken, productRoute)
-app.use('/api/sale',verifyToken, saleRoute)
-app.use('/api/location',verifyToken, locationRoute)
-app.use('/api/employee',verifyToken, employeeRoute)
-app.use('/api/role',verifyToken, roleRoute)
-app.use('/api/transfer',verifyToken, transferRoute)
-app.use('/api/line',verifyToken, lineRoute)
 
 export default app
